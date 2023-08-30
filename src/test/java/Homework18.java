6568f8b386c8b7a84b4c08a708ef58625df5dd5a
abisola2013 <105329548+abisola2013@users.noreply.github.com>
2023-08-29 23:19:39 -0500

    homework 18

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest{
@@ -5,7 +8,30 @@ public class Homework18 extends BaseTest{
     @Test
     public void playSong(){
         loginWithCorrectCredentials();
+        clickOnAllSongs();
+        selectSong();
+        playASong();
+        soundBarVisible();
+    }
 
 
+
+
+    public void soundBarVisible() {
+        WebElement soundBar = driver.findElement(By.cssSelector("#mainFooter .media-info-wrap .other-controls  button:nth-child(2)  img"));
+        Assert.assertTrue(soundBar.isDisplayed());
+    }
+    public void playASong() {
+        WebElement playButton = driver.findElement(By.cssSelector("#mainFooter .side.player-controls .play"));
+        playButton.click();
+    }
+    public void selectSong() {
+        WebElement clickOnSong = driver.findElement(By.cssSelector("#songsWrapper .item-container  tr.song-item.playing.selected  td.title"));
+        clickOnSong.click();
+    }
+
+    public void clickOnAllSongs() {
+        WebElement allSongs = driver.findElement(By.cssSelector("#sidebar section.music li:nth-child(3) a"));
+        allSongs.click();
     }
 }
