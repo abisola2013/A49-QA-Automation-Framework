4d275d986edf51728ed37a9bfd6cb9e19aa85b75
abisola2013 <105329548+abisola2013@users.noreply.github.com>
2023-09-07 18:39:10 -0500

    homework19

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -16,14 +16,16 @@ public class Homework19 extends BaseTest {
 
 
     public void newPlayList(){
-        WebElement newPlayList = driver.findElement(By.cssSelector("#playlists :nth-child(4) a"));
+        WebElement newPlayList = driver.findElement(By.cssSelector("#playlists :nth-child(4)  a"));
         newPlayList.click();
 
     }
     public void deleteNewPlayList(){
         WebElement deletePlayList = driver.findElement(By.cssSelector("[data-testid='playlist-context-menu-delete-70382']"));
         WebElement confirmDeleteCommand = driver.findElement(By.cssSelector("body :nth-child(6)  button.ok"));
-       confirmDeleteCommand.click();
+
+        deletePlayList.click();
+        confirmDeleteCommand.click();
 
     }
     public void notificationDeleteNewPlaylist(){
