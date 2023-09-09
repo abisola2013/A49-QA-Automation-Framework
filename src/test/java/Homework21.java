3c1b2752dee04241dfeca94d138c0fbcb02e7717
abisola2013 <105329548+abisola2013@users.noreply.github.com>
2023-09-08 23:39:12 -0500

    homework21

---
+++ src/test/java/Homework21.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class Homework21 extends BaseTest {
+
+    @Test
+    public void renamePlaylist(){
+    loginWithCorrectCredentials();
+    clickCreatePlaylistBtn();
+    doubleClickNewPlayList();
+    enterNewPlaylist();
+    clickOnNewPlayList();
+    renameNewPlaylist();
+
+
+
+}
+
+    public void renameNewPlaylist() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("")));
+
+    }
+
+    public void clickOnNewPlayList() {
+        WebElement editPlaylist =wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("")));
+        editPlaylist.click();
+
+
+
+    }
+
+    public void doubleClickNewPlayList() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(""))).click();
+    }
+
+    public void clickCreatePlaylistBtn() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[ data-testid='sidebar-create-playlist-btn'] ")))
+                .click();
+    }
+
+    public void enterNewPlaylist() {
+        WebElement newPlayListName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(" ")));
+        newPlayListName.sendKeys(Keys.chord(Keys.CONTROL,"A",Keys.BACK_SPACE));
+        newPlayListName.sendKeys("GreatPlaylist");
+        newPlayListName.sendKeys(Keys.ENTER);
+
+    }
+
+
+}
