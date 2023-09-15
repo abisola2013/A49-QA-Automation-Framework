92bb887fb02fd73288e5aaf14f3f305e702de36e
abisola2013 <105329548+abisola2013@users.noreply.github.com>
2023-09-15 18:21:58 -0500

    homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -22,7 +23,9 @@ public class BaseTest {
 
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+
+//        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
     }
 
     @BeforeMethod
@@ -32,8 +35,9 @@ public class BaseTest {
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--disable-notifications");
         options.addArguments("--start-maximized");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver = new ChromeDriver(options);
+        driver= new FirefoxDriver();
+          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get(url);
         //removed extra lines, our @BeforeMethod annotated will setup our WebDriver and navigate to Koel
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,14 +1,33 @@
 import Pages.LoginPage;
 import Pages.PlaylistPage;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest {
+    LoginPage loginPage;
+
+    @BeforeMethod
+    void setup() {
+        loginPage = new LoginPage(driver);
+        driver.get(url);
+    }
+
     @Test
     public void loginValidEmailPassword() {
-        LoginPage loginPage = new LoginPage(driver);
+
         loginPage.loginWithCorrectCredentials();
     }
 
-    }
+//    @Test
+//    public void inavlidPasswordEmail(){
+//
+//        loginPage.provideEmail("").providePassword("").clickSubmit("");
+//    }
 
+//    @Test
+//    public void emptyEmail() {
+//
+//        loginPage.provideEmail("").providePasword("").clickSubmit("");
+//    }
+}
 
