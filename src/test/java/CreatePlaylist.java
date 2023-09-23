7ca9818a59f4ae04a18bdcad2002440b721a2745
abisola2013 <105329548+abisola2013@users.noreply.github.com>
2023-09-23 17:59:29 -0500

    homework29

--- src/test/java/CreatePlaylist.java
+++ src/test/java/CreatePlaylist.java
@@ -1,11 +1,5 @@
-import Pages.HomePage;
 import Pages.LoginPage;
 import Pages.PlaylistPage;
-import Pages.Profilepage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class CreatePlaylist extends BaseTest {
--- src/test/java/Pages/ProfilePage.java
+++ src/test/java/Pages/ProfilePage.java
@@ -1,17 +1,13 @@
 package Pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 import java.util.UUID;
 
-public class Profilepage extends BasePage {
+public class ProfilePage extends BasePage {
 
     @FindBy(css ="[ name='name']")
     WebElement provideProfileName;
@@ -26,11 +22,11 @@ public class Profilepage extends BasePage {
     @FindBy(css ="a.view-profile>span")
     WebElement actualProfileName;
 
-    public Profilepage(WebDriver givenDriver) {
+    public ProfilePage(WebDriver givenDriver) {
         super(givenDriver);
     }
 
-    public Profilepage provideProfileName(String random) {
+    public ProfilePage provideProfileName(String random) {
         wait.until(ExpectedConditions.visibilityOf(provideProfileName));
       provideProfileName.clear();
       provideProfileName.sendKeys(random);
@@ -41,29 +37,26 @@ public class Profilepage extends BasePage {
         return UUID.randomUUID().toString().replace("", "");
     }
 
-    public Profilepage provideCurrentPassword(String password) {
+    public ProfilePage provideCurrentPassword(String password) {
        wait.until(ExpectedConditions.visibilityOf(currentPassword));
         currentPassword.clear();
         currentPassword.sendKeys(password);
         return this;
     }
 
-    public Profilepage clickSaveButton() {
+    public ProfilePage clickSaveButton() {
         wait.until(ExpectedConditions.visibilityOf(clickSaveButton));
         clickSaveButton.click();
         return this;
     }
 
-    public Profilepage waitForPopUpMessage() {
+    public ProfilePage waitForPopUpMessage() {
         wait.until(ExpectedConditions.visibilityOf(popUpMessage));
         return this;
     }
     public String getActualProfileName() {
         wait.until(ExpectedConditions.visibilityOf(actualProfileName));
             return actualProfileName.getText();
-
-
-
 
         }
    }
